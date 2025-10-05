04d91220bc51e51295d73c442ece36cd5f41a89a
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-10-05 21:09:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 //your JS code here. If required.
+const table_=document.querySelector("#output");
+let row1=table_.insertRow(-1)
+let cell1=row1.insertCell(0)
+// let cell2=row1.insertCell(1)
+// cell2.textContent="xyz"
+cell1.textContent="Loading...";
+cell1.colSpan=2;
+
+function getRandomInteger(min, max) {
+  min = Math.ceil(min); // Ensure min is an integer
+  max = Math.floor(max); // Ensure max is an integer
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+let promise1= new Promise((resolve,reject)=>{
+	let time_taken=getRandomInteger(1,3);
+setTimeout(()=>{
+	resolve(time_taken);
+},time_taken*1000);
+})
+let promise2= new Promise((resolve,reject)=>{
+	let time_taken=getRandomInteger(1,3);
+setTimeout(()=>{
+	resolve(time_taken);
+},time_taken*1000);
+})
+let promise3= new Promise((resolve,reject)=>{
+	let time_taken=getRandomInteger(1,3);
+setTimeout(()=>{
+	resolve(time_taken);
+},time_taken*1000);
+})
+Promise.all([promise1,promise2,promise3]).then((data)=>{
+	table_.innerHTML="";
+	for(let x=0;x<data.length;x++)
+		{
+		let row_new=table_.insertRow(-1);
+		let new_cell_1=row_new.insertCell(0);
+		let new_cell_2=row_new.insertCell(1);
+			new_cell_1.textContent=`Promise ${x+1}`
+			new_cell_2.textContent=`${data[x]}`
+		}
+
+
+	})
+
+
